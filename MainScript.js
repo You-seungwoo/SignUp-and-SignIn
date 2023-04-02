45b117f8f9c2699d42859958117e6d9b694f6cb2
You-seungwoo <128699544+You-seungwoo@users.noreply.github.com>
2023-04-03 07:43:08 +0900

    Update MainScript.js

--- MainScript.js
+++ MainScript.js
@@ -1,6 +1,6 @@
 // import Enmap from "./enmap.js"; enmap DB 불러오기
 
-const Button1 = document.getElementById('signupbutton')
+const Button1 = document.getElementById('signupbutton')// sign up 버튼
 
 Button1.addEventListener('click', function() {
 	const name = document.getElementById('name').value;
@@ -12,4 +12,10 @@ Button1.addEventListener('click', function() {
 		alert("비밀번호 확인이 올바르지 않습니다!")
 		return
 	}
+	
+	if (name.length < 3) {
+		alert("이름 글자 수는 3글자 이상입니다!")
+		return
+	}
+	
 });
